7233f5ea2ab23eaaf50c975138e3f0820101c416
Кувалин Влад <cuvka000@yandex.ru>
2023-07-23 21:23:57 +0300

    Закончил тестить handler

--- app/src/main/java/com/example/coroutinestart/MainActivity.kt
+++ app/src/main/java/com/example/coroutinestart/MainActivity.kt
@@ -1,13 +1,45 @@
 package com.example.coroutinestart
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.widget.Toast
 import androidx.core.view.isVisible
 import com.example.coroutinestart.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+
+    //region Комментарий
+    /*
+    1. В целях решения проблемы с передачей данных из субпотоков в главный используется класс: Handler.
+    Объект этого класса можно создать на главном потоке (Main-thread), а затем из субпотоков
+    (Background thread), ему можно передать объекты Runnable.
+    И тогда метод Runnable.run() будет вызван на главном потоке.
+
+    2. Кстати, важно. Чтобы создать отдельную ветку в субпотоке, то нужно вызывать метод:
+        Looper.prepare() - лучше почитать, если буду использовать.
+    */
+    //endregion
+    //region sendMessage
+    /*
+    Так же handler может принимать не только runnable, но и объект message.
+    Но чтобы принимать значения, нужно переопределить метод handleMessage при создании handler
+
+    private val handler = object : Handler(){
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            println("HANDLE_msg $msg")
+        }
+    }
+
+    handler.sendMessage(Message.obtain(handler, 0, 17))
+    */
+    //endregion
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -37,19 +69,31 @@ class MainActivity : AppCompatActivity() {
     private fun loadCity(callback: (String) -> Unit) {
         thread {
             Thread.sleep(5000)
-            callback.invoke("Moscow")
+            runOnUiThread {
+                callback.invoke("Moscow")
+            }
         }
     }
 
     private fun loadTemperature(city: String, callback: (Int) -> Unit) {
         thread {
-            Toast.makeText(
-                this,
-                "Loading temperature for city: $city",
-                Toast.LENGTH_SHORT
-            ).show()
+            Handler(Looper.getMainLooper()).post {
+                Toast.makeText(
+                    this,
+                    "Loading temperature for city: $city",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
             Thread.sleep(5000)
-            callback.invoke(17)
+            //region runOnUiThread - что это?
+            /*
+            Конструкцию "Handler(Looper.getMainLooper()).post" можно упростить аналогичным методом:
+                runOnUiThread (Переводится как: "запусти на главном потоке")
+            */
+            //endregion
+            runOnUiThread {   // Пример для слов выше
+                callback.invoke(17)
+            }
         }
     }
 }
